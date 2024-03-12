8c3507133d2feb910e5d536ec0314fa354411efe
brood brood <brood@desktop.voidworks.cc>
2024-03-12 23:13:00 +0000

    nginx default.rb attempt

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
+
+package 'nginx' do
+  action :install
+end
+
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
